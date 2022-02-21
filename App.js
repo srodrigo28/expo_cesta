b0bff41b73555ae8d8fb750e14920e1570953e9b
Sebastiao Rodrigo <rodrigoexer1@gmail.com>
2022-02-21 13:11:10 -0300

    "update"

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { TouchableOpacity, StatusBar, SafeAreaView, StyleSheet, Text, TextInput, Alert} from 'react-native';
-import Cadastro from './src/telas/Cadastro';
-import Cesta from './src/telas/Cesta';
-import Login2 from './src/telas/Login2';
+import { StatusBar, SafeAreaView} from 'react-native';
+import Login from './src/telas/Login';
 
 export default function App(){
   return(
     <SafeAreaView>
         <StatusBar />
-        {/* <Cesta></Cesta>    */}
-        {/* <Cadastro></Cadastro> */}
-        <Login2/>
+        <Login/>
     </SafeAreaView>
   )}
